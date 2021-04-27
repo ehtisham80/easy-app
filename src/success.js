188e9e740b39fa568a55d6f9cf24ee83b92573d9
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2021-04-27 03:20:29 +0200

    created success component
    created success component

---
+++ src/success.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Success extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Message Send successfully</h1>
+                <p>Thank you for contacting us. We received your message and We will get back to soon. </p>
+            </div>
+        )
+    }
+}
